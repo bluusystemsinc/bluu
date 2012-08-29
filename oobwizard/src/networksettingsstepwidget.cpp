67755b8fc865ffbb2114335fa26605b20d6d7e53
Zlatko Gjuroski <zgjuroski@milosolutions.com>
2012-08-29 16:07:48 +0200

    added dns fields to /etc/resolv.conf file! this wizard will need to be started with sudocd ..

--- oobwizard/src/networksettingsstepwidget.cpp
+++ oobwizard/src/networksettingsstepwidget.cpp
@@ -1,6 +1,7 @@
 #include "networksettingsstepwidget.h"
 #include "QFile"
 #include <QTextStream>
+#include <QDebug>
 
 NetworkSettingsStepWidget::NetworkSettingsStepWidget(QWidget *parent) :
     QWidget(parent)
@@ -29,10 +30,26 @@ void NetworkSettingsStepWidget::createConnetion()
     {
       out << "lanName=\"$(ifconfig | awk \"/^eth/ {print \\$1}\")\"\n";
       out << "/sbin/ifconfig $lanName " << staticIpAddrlineEdit->text() << " netmask " << subnetMaskLineEdit->text() << " broadcast " << defaultGatewayLineEdit->text() << "\n";
+      out << "/sbin/route add default gw " << defaultGatewayLineEdit->text() << "\n";
     }
     file.close();
 
+    if(dnsNoRadioBtn->isChecked())
+    {
+        QString nameserverFileName("/etc/resolv.conf");
+        QFile nameserverFile(nameserverFileName);
 
+        bool ret = nameserverFile.open(QIODevice::Append | QIODevice::Text);
+        if(ret)
+            qDebug() << "ok\n";
+        else
+            qDebug() << "not ok\n";
+        QTextStream out1(&nameserverFile);
+        out1 << "nameserver " << defaultGatewayLineEdit->text() << "\n";
+        out1 << "nameserver " << dnsServer1LineEdit->text() << "\n";
+        out1 << "nameserver " << dnsServer2LineEdit->text() << "\n";
+        nameserverFile.close();
+    }
 
 //    QString program("testRouter.sh");
 
