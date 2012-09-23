bf19547daf927983dcbce452ba9135151a7e81d8
Jesus Fernandez <jfernandez@milosolutions.com>
2012-09-23 13:07:59 +0200

    First version of the sensorbase class

--- caim_sensorbase/include/abstractsensor.h
+++ caim_sensorbase/include/abstractsensor.h
@@ -3,13 +3,21 @@
 
 #include <QObject>
 
+class QTextStream;
+
 class AbstractSensor : public QObject
 {
     Q_OBJECT
 
 public slots:
+    void serialize(QTextStream *stream);
+
 signals:
     void dataAvailable();
 };
+
+typedef QList<AbstractSensor*> AbstractSensorList;
+
+AbstractSensorList instances();
 
 #endif // ABSTRACTSENSOR_H
