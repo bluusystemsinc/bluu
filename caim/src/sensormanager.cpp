4c9c838575f36a9c872644e104695fea358fb11a
Zlatko Gjuroski <zgjuroski@milosolutions.com>
2012-10-01 12:46:50 +0200

    delete last commit

--- caim/src/sensormanager.cpp
+++ caim/src/sensormanager.cpp
@@ -12,10 +12,6 @@
 SensorManager::SensorManager(QObject *parent) :
     QObject(parent)
 {
-    timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()), this,SLOT(timerSlot()));
-
-    timer->start(5000);
 }
 
 void SensorManager::loadSensorLibraries()
@@ -79,21 +75,4 @@ void SensorManager::loadSensorLibraries()
             delete library;
         }
     }
-}
-
-void SensorManager::timerSlot()
-{
-    QFile file("/dev/urandom");
-    quint32 x;
-
-    if(!file.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Can not open for read";
-    }
-
-    QDataStream in(&file);
-    in >> x;
-
-    qDebug() << "Timer elipsed" << x << "\n";
-
 }
