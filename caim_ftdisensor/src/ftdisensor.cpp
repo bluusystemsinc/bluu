bd1fd174775ac58226ac6b0d35b4775fd33e0891
Zlatko Gjuroski <zgjuroski@milosolutions.com>
2012-10-01 14:26:19 +0200

    fake sensor data on timer interval

--- caim_ftdisensor/src/ftdisensor.cpp
+++ caim_ftdisensor/src/ftdisensor.cpp
@@ -3,20 +3,34 @@
 #include <QDebug>
 #include <QThread>
 #include <QStringList>
+#include <QFile>
 
 addVersionInfo()
 
 extern "C" AbstractSensorList instances()
 {
     AbstractSensorList list;
-
     list.append(new FtdiSensor);
+
+    FtdiSensor *timeoutSensor = new FtdiSensor(2000);
+    list.append(timeoutSensor);
+
+
     return list;
 }
 
 FtdiSensor::FtdiSensor(QObject *parent) :
     AbstractSensor(parent)
 {
+}
+
+FtdiSensor::FtdiSensor(int timeout, QObject *parent) :
+    AbstractSensor(parent)
+{
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this,SLOT(timerSlot()));
+
+    timer->start(timeout);
 }
 
 void FtdiSensor::plug()
@@ -26,4 +40,21 @@ void FtdiSensor::plug()
 
 void FtdiSensor::serialize(QTextStream */*stream*/)
 {
+}
+
+void FtdiSensor::timerSlot()
+{
+    QFile file("/dev/urandom");
+    quint32 x;
+
+    if(!file.open(QIODevice::ReadOnly))
+    {
+        qDebug() << "Can not open for read\n";
+    }
+
+    QDataStream in(&file);
+    in >> x;
+
+    qDebug() << "Timer elipsed" << x << "\n";
+
 }
--- caim_ftdisensor/src/ftdisensor.h
+++ caim_ftdisensor/src/ftdisensor.h
@@ -2,16 +2,23 @@
 #define FTDISENSOR_H
 
 #include <abstractsensor.h>
+#include <QTimer>
 
 class FtdiSensor : public AbstractSensor
 {
     Q_OBJECT
 public:
-    explicit FtdiSensor(QObject *parent = 0);
+
+    explicit FtdiSensor(int timeout, QObject *parent = 0);
+    FtdiSensor(QObject *parent = 0);
+
+    QTimer *timer;
 
     Q_INVOKABLE virtual void plug();
 
     virtual void serialize(QTextStream *stream);
+public slots:
+    void timerSlot();
 };
 
 #endif // FTDISENSOR_H
