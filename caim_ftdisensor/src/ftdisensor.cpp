62046fcaa7f67c2c73c01a734e085e160ca965da
Jesus Fernandez <jfernandez@milosolutions.com>
2012-10-03 00:12:22 +0200

    FT_Open called

--- caim_ftdisensor/src/ftdisensor.cpp
+++ caim_ftdisensor/src/ftdisensor.cpp
@@ -26,6 +26,7 @@ bool FtdiSensor::plug()
 {
     FT_STATUS ftStatus;
     DWORD numDevices = 0;
+    FT_DEVICE_LIST_INFO_NODE *devInfo;
 
     qDebug()<<"I'm at working at"<<QThread::currentThread();
 
@@ -36,9 +37,32 @@ bool FtdiSensor::plug()
         return false;
     }
     qDebug()<<"FT_CreateDeviceInfoList"<<ftStatus;
+    devInfo = new FT_DEVICE_LIST_INFO_NODE[numDevices];
+    ftStatus = FT_GetDeviceInfoList(devInfo, &numDevices);
+    if(FT_OK != ftStatus)
+    {
+        qCritical()<<"Error FT_GetDeviceInfoList"<<ftStatus;
+        return false;
+    }
     for(DWORD i = 0; i < numDevices; i++)
     {
+        FT_DEVICE_LIST_INFO_NODE *node = devInfo + i;
+
+        ftStatus = FT_Open(i, &(node->ftHandle));
+        if(FT_OK != ftStatus)
+        {
+            qCritical()<<"Error FT_Open"<<ftStatus;
+            return false;
+        }
+
         qDebug()<<"Device"<<i;
+        qDebug()<<" Flags"<<node->Flags;
+        qDebug()<<" Type"<<node->Type;
+        qDebug()<<" ID"<<node->ID;
+        qDebug()<<" LocId"<<node->LocId;
+        qDebug()<<" Serial number"<<node->SerialNumber;
+        qDebug()<<" Description"<<node->Description;
+        qDebug()<<" ftHandle"<<node->ftHandle;
     }
     return true;
 }
