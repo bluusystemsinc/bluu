d0d1be832401abbb29d3be02d71c1d8f8abcb139
Jakub Wiśniowski <restless.being@gmail.com>
2013-02-27 14:13:11 +0100

    debug=false environments additional configuration settings

--- bluu-web/project/production.py
+++ bluu-web/project/production.py
@@ -3,9 +3,12 @@ from project.settings import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-MANAGERS = (
-    ('Jakub Wisniowski', 'jwisniowski@milosolutions.com'),
-)
+EMAIL_HOST='smtp.emailsrvr.com'
+EMAIL_HOST_USER='noreply@bluusystems.com'
+EMAIL_HOST_PASSWORD='set this in local_settings.py'
+DEFAULT_FROM_EMAIL='noreply@bluusystems.com'
+
+ALLOWED_HOSTS=['198.61.203.159']
 
 try:
     from local_settings import *
