62afa821122baa5db5a1a47a554dbc1655210077
Jakub Wiśniowski <jwisniowski@milosolutions.com>
2013-02-28 23:20:55 +0000

    test.py config

--- bluu-web/project/test.py
+++ bluu-web/project/test.py
@@ -8,6 +8,19 @@ EMAIL_HOST_USER='noreply@bluusystems.com'
 EMAIL_HOST_PASSWORD='set this in local_settings.py'
 DEFAULT_FROM_EMAIL='noreply@bluusystems.com'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/opt/webapps/test/bluu/bluu-web/bluu.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+COMPRESS_ENABLED=True
+
 ALLOWED_HOSTS=['test.web.bluusystems.com', '198.61.203.159']
 
 try:
