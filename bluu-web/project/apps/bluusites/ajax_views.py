46aab47072bb3f92c5c4be11f94fd6e8fabe3ad3
Jakub Wiśniowski <restless.being@gmail.com>
2013-03-13 12:32:40 +0100

    excluded webservice user from site access list

--- bluu-web/project/apps/bluusites/ajax_views.py
+++ bluu-web/project/apps/bluusites/ajax_views.py
@@ -112,7 +112,8 @@ class BluuSiteAccessListJson(BaseDatatableView):
 
         self.bluusite = self.get_site(kwargs.get('pk'))
 
-        qs = BluuSiteAccess.objects.filter(site=self.bluusite)
+        qs = BluuSiteAccess.objects.filter(site=self.bluusite).\
+                exclude(user__username__startswith=settings.WEBSERVICE_USERNAME_PREFIX)
         # number of records before filtering
         total_records = qs.count()
         qs = self.filter_queryset(qs)
