620f21ba8081cdfecd8601ad09b31291b4360f62
Jakub Wiśniowski <restless.being@gmail.com>
2013-03-23 02:01:51 +0100

    celery config for test instance

--- bluu-web/project/test.py
+++ bluu-web/project/test.py
@@ -23,6 +23,24 @@ COMPRESS_ENABLED = True
 
 ALLOWED_HOSTS=['test.web.bluusystems.com', '198.61.203.159']
 
+# CELERY
+from celery.schedules import crontab
+djcelery.setup_loader()
+
+BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_IMPORTS = ('bluusites.tasks', 'companies.tasks') 
+CELERYBEAT_SCHEDULE = {
+    'cleanup_siteacess': {
+        'task': 'bluusites.tasks.cleanup_siteaccess',
+        'schedule': crontab(hour='*', minute='*/1'),
+    },
+    'cleanup_companyaacess': {
+        'task': 'companies.tasks.cleanup_companyaccess',
+        'schedule': crontab(hour='*', minute='*/1'),
+    },
+}
+
 try:
     from local_settings import *
 except ImportError:
