3c792d81a3ad35ea672cc7900ca6c66666d86256
Jakub Wiśniowski <restless.being@gmail.com>
2013-04-16 01:12:48 +0200

    alerts, forms

--- bluu-web/project/apps/alerts/forms.py
+++ bluu-web/project/apps/alerts/forms.py
@@ -8,12 +8,12 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from crispy_forms import layout
 from crispy_forms.bootstrap import FormActions
-from .models import (Alert, UserAlertDevice)
+from .models import (Alert, UserAlertDevice, UserAlertConfig)
 
 
-class DurationForm(forms.Form):
-    duration = forms.IntegerField()
-    unit = forms.ChoiceField(choices=Alert.UNITS)
+class DurationForm(forms.ModelForm):
+    #duration = forms.IntegerField()
+    #unit = forms.ChoiceField(choices=Alert.UNITS)
 
     def __init__(self, device_type, alert, **kwargs):
         self.helper = FormHelper()
@@ -24,4 +24,27 @@ class DurationForm(forms.Form):
 
         super(DurationForm, self).__init__(**kwargs)
         self.helper.layout = layout.Layout(durak, unit)
+
+    class Meta:
+        model = UserAlertConfig
+        fields = ('duration', 'unit')
+
+
+class NotificationForm(forms.ModelForm):
+    #duration = forms.IntegerField()
+    #unit = forms.ChoiceField(choices=Alert.UNITS)
+
+    def __init__(self, device_type, alert, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+
+        text = layout.Field('text_notification') #, css_class="input-small unit", template='alerts/conf_unit_template.html')
+        email = layout.Field('email_notification') #, css_class="input-mili duration", maxlength="3", template='alerts/conf_duration_template.html')
+
+        super(NotificationForm, self).__init__(**kwargs)
+        self.helper.layout = layout.Layout(text, email)
+
+    class Meta:
+        model = UserAlertConfig
+        fields = ('text_notification', 'email_notification')
 
--- bluu-web/project/apps/alerts/templatetags/alert_tags.py
+++ bluu-web/project/apps/alerts/templatetags/alert_tags.py
@@ -6,7 +6,8 @@ from django.template import Library
 from django.utils.translation import ugettext as _
 
 from devices.models import Device
-from ..forms import DurationForm
+from ..forms import (DurationForm, NotificationForm)
+from ..models import UserAlertConfig
 
 register = Library()
 
@@ -26,13 +27,15 @@ def alerts_for_device_type(context, bluusite, device_type):
 @register.tag(name="alertbox")
 def do_alertbox_for_alert(parser, token):
     try:
-        tag_name, bluusite, device_type, alert = token.split_contents()
+        tag_name, user, bluusite, device_type, alert = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError(_("%(tag_name)s tag requires exactly three arguments") % {'tag_name': token.contents.split()[0]})
-    return AlertBoxNode(bluusite, device_type, alert)
+    return AlertBoxNode(user, bluusite, device_type, alert)
+
 
 class AlertBoxNode(template.Node):
-    def __init__(self, bluusite, device_type, alert):
+    def __init__(self, user, bluusite, device_type, alert):
+        self.user_var = template.Variable(bluusite)
         self.bluusite_var = template.Variable(bluusite)
         self.device_type_var = template.Variable(device_type)
         self.alert_var = template.Variable(alert)
@@ -41,13 +44,23 @@ class AlertBoxNode(template.Node):
         # for each alert render all devices of this type in this site
         # for each device set value of its current alert setting (if set)
         request = context['request']
+        user = self.user_var.resolve(context)
         bluusite = self.bluusite_var.resolve(context)
         device_type = self.device_type_var.resolve(context)
         alert = self.alert_var.resolve(context)
         devices = Device.objects.filter(bluusite=bluusite,
                                         device_type=device_type)
+        try:
+            instance = UserAlertConfig.objects.get(user=user, alert=alert)
+        except UserAlertConfig.DoesNotExist:
+            instance = None
         form = DurationForm(device_type=device_type, alert=alert,
-                initial={'time': 0, 'unit': 's'})
+                instance=instance, initial={'duration': 0, 'unit': 'h'})
+
+        nform = NotificationForm(device_type=device_type, alert=alert,
+                initial={'text_notification': True,
+                         'email_notification': False})
+
 
         t = template.loader.get_template('alerts/_conf_{0}_{1}.html'.\
                    format(device_type.name.lower(), alert.alert_type.lower()))
@@ -55,6 +68,7 @@ class AlertBoxNode(template.Node):
         ctx = template.Context({'alert': alert,
                                 'user': request.user,
                                 'form': form,
+                                'nform': nform,
                                 'device_type': device_type,
                                 'devices': devices},
                                autoescape=context.autoescape)
